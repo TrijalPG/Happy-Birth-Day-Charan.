928df65ba411426cdd40f18d5983b7033549cd34
TrijalPG <77110312+TrijalPG@users.noreply.github.com>
2021-01-13 11:48:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Charan" && b=="13-01-2021")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
